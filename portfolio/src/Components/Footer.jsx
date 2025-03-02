380312227c2fb38f4a22b6dd5b29f358bef06c58
mukuvi <jamesmngandu@gmail.com>
2025-03-02 19:11:03 +0300

    portfolio

---
+++ portfolio/src/Components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div>
+      <div className="foot-left">
+        <h1>Developer</h1>
+      </div>
+      <div className="foot-right">
+        <p>
+          &copy; {new Date().getFullYear()} James Ngandu. All rights reserved.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ portfolio/src/Components/Header.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <div className="header-logo">
+        <h1>James Ngandu</h1>
+      </div>
+      <div className="header-navs">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Skills</li>
+          <li>Projects</li>
+          <li>Services</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
