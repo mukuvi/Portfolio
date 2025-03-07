803ba9bfe1c703d3638eb44bc6f24ec1c0f34c53
mukuvi <jamesmngandu@gmail.com>
2025-03-07 23:51:28 +0300

    commit

--- portfolio/src/Components/Footer.jsx
+++ portfolio/src/Components/Footer.jsx
@@ -1,10 +1,22 @@
 import React from "react";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
 
 function Footer() {
   return (
     <div className="footer-sect">
       <div className="foot-left">
-        <h1>Developer</h1>
+        <div className="social-icons">
+          <a href="https://github.com/mukuvi">
+            <FaGithub size={30} className="social-icon" />
+          </a>
+          <a href="https://twitter.com/Mukuvi_">
+            <FaXTwitter size={30} className="social-icon" />
+          </a>
+          <a href="https://www.linkedin.com/in/james-ngandu">
+            <FaLinkedin size={30} className="social-icon" />
+          </a>
+        </div>
       </div>
       <div className="foot-right">
         <p>
