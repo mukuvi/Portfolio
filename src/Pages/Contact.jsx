12406ab0e2a2505b3ef12ee25f7103251ee6ccf1
mukuvi <jamesmngandu@gmail.com>
2025-03-18 01:37:05 +0300

    mukuvi

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "../Components/Header";
 import Footer from "../Components/Footer";
-import MyImage from "/public/mukuvi.jpeg";
+import MyImage from "/mukuvi.jpeg";
 
 export default function BasicForm() {
   return (
